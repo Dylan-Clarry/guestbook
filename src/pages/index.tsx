2a5214b4e0778de66057de7c73e95705a802d370
Dylan <dylan.imail@gmail.com>
2022-12-12 10:00:42 -0500

    Project init; NextAuth with Discord setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,31 @@
+import { signIn, signOut, useSession } from 'next-auth/react';
+
+const Home = () => {
+    const { data: session, status } = useSession();
+
+    if(status === "loading") {
+        return <main>Loading...</main>;
+    }
+
+    return (
+        <main>
+            <h1>Guestbook</h1>
+            <div>
+                {session ? (
+                    <>
+                        <p>Hi {session.user?.name}</p>
+                        <button onClick={() => signOut()}>
+                            Logout
+                        </button>
+                    </>
+                ) : (
+                    <button onClick={() => signIn("discord")}>
+                        Login with Discord
+                    </button>
+                )}
+            </div>
+        </main>
+    );
+};
+
+export default Home;
